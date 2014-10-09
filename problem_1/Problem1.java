b9c6b455a688b9df61f9d1fe9dfc5f682fa4ab69
Alexandre Laporte <alaporte.live@gmail.com>
2014-10-08 21:55:48 -0400

    Bug Fix

--- problem_1/Problem1.java
+++ problem_1/Problem1.java
@@ -1,12 +1,12 @@
-public class problem1 {
+public class Problem1 {
 	/** Computes the sum of the numbers
-	 * divisible by 3 or 5 bellow (including) 1000;
+	 * divisible by 3 or 5 bellow (excluding) 1000;
 	 * @param (none)
 	 * @return sum to standard out. 
 	 */ 
 	public static void main(String[] args) { 
 		int sum = 0;
-		for(int i = 1; i <= 1000; i++)  { 
+		for(int i = 1; i < 1000; i++)  { 
 				if(i%5==0 || i%3 == 0)
 						sum +=i; 
 		}
